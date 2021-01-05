fd1178c0849f4743d20e9e74a731b5d6cff6bc10
jmukul <dev.jmukul@users.noreply.github.com>
2021-01-05 15:59:22 +0530

    created boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        dark : '#606060',
+        black: '#030303',
+        red: '#FF0000',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

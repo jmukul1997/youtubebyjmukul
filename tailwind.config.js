529b0c2bee112a14dc646b8dfce91e132f5595ac
jmukul <dev.jmukul@users.noreply.github.com>
2021-01-05 21:58:35 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
         dark : '#606060',
         black: '#030303',
         red: '#FF0000',
+        middle: '#F9F9F9',
       },
+      spacing: {
+        '128' : '575px',
+      }
     },
   },
   variants: {
